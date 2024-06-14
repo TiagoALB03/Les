4cc72d16ce1f0da30df525ecfe9322d4ee418328
Smacker23 <alexdscarvalho@gmail.com>
2024-06-14 22:30:57 +0100

    Inicio dos botões questionário

--- questionario/tables.py
+++ questionario/tables.py
@@ -42,13 +42,20 @@ class QuestionarioTable(django_tables.Table):
                 """)
 
     def render_acoes(self, record):
-        primeiro_botao = """<span class="icon"></span>"""
-        segundo_botao = """<span class="icon"></span>"""
-        terceiro_botao = """<span class="icon"></span>"""
+        editar_botao = f"""
+                                        <a data-tooltip="editar" onclick="alert2.render('O questionário não pode ser editado no estado {record.getQuestionarioEstado}.<strong> Não o pode apagar.</strong>','{reverse('questionarios:consultar-estados-admin')}')">
+                                             <span class="icon">
+                                      <i class="fas fa-pencil-alt aria-hidden="true" style="color: #F4B400"></i>
+                                 </span>
+                                        </a> 
+                                    """
+        responder_botao = """<span class="icon"></span>"""
+        publicar_botao = """<span class="icon"></span>"""
+        despublicar_botao = """<span class="icon"></span>"""
         botao_apagar = """<span class="icon"></span>"""
         t_botao = """<span class="icon"></span>"""
         if record.getQuestionarioEstado == "concluido":
-            primeiro_botao = f"""
+            editar_botao = f"""
                                <a data-tooltip="Arquivar" href="{reverse('questionarios:arquivar-questionario', args=[record.getQuestionarioID])}">
                                    <span class="icon">
                                        <i class="fas fa-archive" aria-hidden="true" style="color: #F4B400"></i>
@@ -56,7 +63,7 @@ class QuestionarioTable(django_tables.Table):
                                </a>
                            """
         if record.getQuestionarioEstado == "validado":
-            segundo_botao = f"""
+            responder_botao = f"""
                                <a data-tooltip="Responder" href="{reverse('questionarios:responder-questionario', args=[record.getQuestionarioID])}">
                                    <span class="icon">
                                         <i class="fas fa-reply" aria-hidden="true" style="color: #3273DC"></i>
@@ -64,14 +71,14 @@ class QuestionarioTable(django_tables.Table):
                                </a>
                            """
         if record.getQuestionarioEstado == "validado":
-            terceiro_botao = f"""
+            publicar_botao = f"""
                                <a data-tooltip="Publicar" href="{reverse('questionarios:publicar-questionario', args=[record.getQuestionarioID])}">
                                    <span class="icon">
                                         <i class="fas fa-upload" style="color:#DB2323"></i>
                                    </span>
                                </a>
                            """
-            primeiro_botao = f"""
+            editar_botao = f"""
                                <a data-tooltip="editar" href="{reverse('questionarios:editar-questionario', args=[record.getQuestionarioID])}">
                                  <span class="icon">
                                       <i class="fas fa-pencil-alt aria-hidden="true" style="color: #F4B400"></i>
@@ -80,14 +87,14 @@ class QuestionarioTable(django_tables.Table):
                          """
 
         if record.getQuestionarioEstado == "publicado":
-            segundo_botao = f"""
+            responder_botao = f"""
                                <a data-tooltip="Responder" href="{reverse('questionarios:responder-questionario', args=[record.getQuestionarioID])}">
                                    <span class="icon">
                                         <i class="fas fa-reply" aria-hidden="true" style="color: #3273DC"></i>
                                    </span>
                                </a>
                            """
-            terceiro_botao = f"""
+            despublicar_botao = f"""
                                 <a data-tooltip="Validar" href="{reverse('questionarios:validar-questionario', args=[record.getQuestionarioID])}">
                                     <span class="icon">
                                         <i class="fas fa-check" aria-hidden="true" style="color: #1EE232"></i>
@@ -96,14 +103,14 @@ class QuestionarioTable(django_tables.Table):
                             """
 
         if record.getQuestionarioEstado == "pendente":
-            segundo_botao = f"""
+            responder_botao = f"""
                                <a data-tooltip="Responder" href="{reverse('questionarios:responder-questionario', args=[record.getQuestionarioID])}">
                                    <span class="icon">
                                         <i class="fas fa-reply" aria-hidden="true" style="color: #3273DC"></i>
                                    </span>
                                </a>
                            """
-            primeiro_botao = f"""
+            editar_botao = f"""
                               <a data-tooltip="editar" href="{reverse('questionarios:editar-questionario', args=[record.getQuestionarioID])}">
                                   <span class="icon">
                                        <i class="fas fa-pencil-alt aria-hidden="true" style="color: #F4B400"></i>
@@ -139,9 +146,10 @@ class QuestionarioTable(django_tables.Table):
         return format_html(f"""
                <div>
                     {botao_apagar}
-                    {primeiro_botao}
-                    {segundo_botao}
-                    {terceiro_botao}
+                    {editar_botao}
+                    {responder_botao}
+                    {publicar_botao}
+                    {despublicar_botao}
                     
                </div>    
            """)
@@ -199,6 +207,7 @@ class EscalasTable(django_tables.Table):
     class Meta:
         model = questionario_escalaresposta
         fields = ('nome', 'valores')
+
 
 class EstadoTable(tables.Table):
     cor = tables.Column('Cor', accessor='getEstadoCor', orderable=False)
